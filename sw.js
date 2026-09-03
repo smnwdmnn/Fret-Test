b04bcd144604cc137b1f5ca972be77d89291a136
smnwdmnn <44921184+smnwdmnn@users.noreply.github.com>
2026-09-03 10:03:46 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="fret-speed-v34-mic-test";
+const CACHE="fret-speed-v35-mic-test";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg?v=25"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
